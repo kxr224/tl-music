ce11000c946886c89130ff8270b2ceb0d9e287ba
cyrus <kxr224@163.com>
2020-07-10 09:50:54 +0800

    增加了旋转

--- src/views/MusicPlay/MusicPlay.vue
+++ src/views/MusicPlay/MusicPlay.vue
@@ -3,7 +3,7 @@
         <div style="position: fixed; top: 0;left: 0;right: 0;bottom: 0;background-color:#f5f5f5;z-index: -1"></div>
         <Header :title="songInfo.title" />
         <div class="cover">
-            <img :src="songInfo.pic_huge" alt="">
+            <img :src="songInfo.pic_huge" alt="" :class="{turn:isPlay}">
         </div>
         <Lrc v-if="songInfo.lrclink" :lrc-link="songInfo.lrclink" />
         <MusicAction :file-link="bitrate.file_link" />
@@ -33,7 +33,8 @@
                 // 歌曲详情
                 songInfo: {},
                 // 歌曲的媒体信息
-                bitrate: {}
+                bitrate: {},
+                isPlay: false
             }
         },
         created() {
@@ -46,6 +47,12 @@
         mounted() {
             this.$refs.audio.addEventListener("timeupdate", () => {
                 this.$store.commit("setCurrentTime", {currentTime: this.$refs.audio.currentTime})
+            })
+            this.$refs.audio.addEventListener("play",  ()=> {   //开始播放时触发
+                this.isPlay = true
+            });
+            this.$refs.audio.addEventListener("pause",  ()=> {   // 暂停时会触发，当播放完一首歌曲时也会触发
+                this.isPlay = false
             })
         },
         computed: {
@@ -71,8 +78,18 @@
         margin-bottom: 50px;
 
         img {
-            border-radius: 10px;
+            border-radius: 50%;
             width: 55%;
+        }
+
+        .turn {
+            animation: spin 12s linear infinite;
+        }
+    }
+
+    @-webkit-keyframes spin {
+        to {
+            transform: rotate(1turn);
         }
     }
 </style>
